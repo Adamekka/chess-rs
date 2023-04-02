1a04087704cd162d647354891ac1c62fb4678b18
Adam Cvikl <adam.wasd4@gmail.com>
2023-04-02 15:26:50 +0200

    create chessboard

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,38 @@ fn main() {
 fn setup(mut commands: Commands) {
     // Camera
     commands.spawn(Camera2dBundle::default());
+    // Chessboard
+    let black_material = Color::rgb(0.0, 0.0, 0.0);
+    let white_material = Color::rgb(1.0, 1.0, 1.0);
+
+    let n_of_squares: u8 = 8;
+    let square_size: f32 = 60.0;
+
+    let board_half_width = square_size * n_of_squares as f32 / 2.0;
+
+    for row in 0..n_of_squares {
+        for column in 0..n_of_squares {
+            let square_pos = Vec2::new(
+                row as f32 * square_size - board_half_width + square_size / 2.0,
+                column as f32 * square_size - board_half_width + square_size / 2.0,
+            );
+            let material = if (row + column) % 2 == 0 {
+                &black_material
+            } else {
+                &white_material
+            };
+            commands.spawn(SpriteBundle {
+                transform: Transform {
+                    translation: square_pos.extend(0.0),
+                    scale: Vec3::new(square_size, square_size, 1.),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: material.clone(),
+                    ..default()
+                },
+                ..default()
+            });
+        }
+    }
 }
