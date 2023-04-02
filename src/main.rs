5e9c8a1a34840104c4856a63d1926c21569e2e56
Adam Cvikl <adam.wasd4@gmail.com>
2023-04-02 15:11:43 +0200

    set custom resolution, window title and make it unresizable

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 use bevy::prelude::*;
 
+const WINDOW_TITLE: &str = "Chess by Adamekka";
+const WINDOW_WIDTH: u16 = 1280;
+const WINDOW_HEIGHT: u16 = 720;
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: WINDOW_TITLE.into(),
+                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                resizable: false,
+                ..Default::default()
+            }),
+            ..default()
+        }))
         .add_startup_system(setup)
         .run();
 }
