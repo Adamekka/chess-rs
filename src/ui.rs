f91e3fd7480c5ba8aeef8aaa62d0caea59fc3afa
Adam Cvikl <adam.wasd4@gmail.com>
2023-04-30 17:49:52 +0200

    `UI`: implement updating turn in UI after each turn

--- src/ui.rs
+++ src/ui.rs
@@ -3,9 +3,13 @@ use crate::*;
 pub struct UIPlugin;
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(init_show_turn_ui_text);
+        app.add_startup_system(init_show_turn_ui_text)
+            .add_system(update_turn_ui_text);
     }
 }
+
+#[derive(Component)]
+struct NextMoveText;
 
 fn init_show_turn_ui_text(
     mut commands: Commands,
@@ -14,32 +18,52 @@ fn init_show_turn_ui_text(
 ) {
     let font: Handle<Font> = asset_server.load("fonts/UbuntuMonoNerdFontCompleteMono.ttf");
 
-    commands.spawn(TextBundle {
-        style: Style {
-            align_self: AlignSelf::FlexEnd,
-            position_type: PositionType::Absolute,
-            position: UiRect {
-                top: Val::Px(5.0),
-                right: Val::Px(5.0),
+    commands
+        .spawn(TextBundle {
+            style: Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    top: Val::Px(5.0),
+                    right: Val::Px(5.0),
+                    ..default()
+                },
                 ..default()
             },
-            ..default()
-        },
-        text: Text::from_section(
-            String::from(format!(
-                "Turn:  {:?}\n       {}",
-                turn.get_color(),
-                turn.get_number_as_ordinal()
-            )),
-            TextStyle {
-                font,
-                font_size: 40.0,
-                color: match turn.get_color() {
-                    PieceColor::White => Color::WHITE,
-                    PieceColor::Black => Color::BLACK,
+            text: Text::from_section(
+                String::from(format!(
+                    "Turn:  {:?}\n       {}",
+                    turn.get_color(),
+                    turn.get_number_as_ordinal()
+                )),
+                TextStyle {
+                    font,
+                    font_size: 40.0,
+                    color: match turn.get_color() {
+                        PieceColor::White => Color::WHITE,
+                        PieceColor::Black => Color::BLACK,
+                    },
                 },
-            },
-        ),
-        ..default()
-    });
+            ),
+            ..default()
+        })
+        .insert(NextMoveText);
+}
+
+fn update_turn_ui_text(turn: Res<Turn>, mut query: Query<&mut Text, With<NextMoveText>>) {
+    if !turn.is_changed() {
+        return;
+    }
+
+    for mut text in query.iter_mut() {
+        text.sections[0].value = String::from(format!(
+            "Turn:  {:?}\n       {}",
+            turn.get_color(),
+            turn.get_number_as_ordinal()
+        ));
+        text.sections[0].style.color = match turn.get_color() {
+            PieceColor::White => Color::WHITE,
+            PieceColor::Black => Color::BLACK,
+        };
+    }
 }
